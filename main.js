e02980608317ffe9a86dd4e06a5235d4b8809118
Grzesiekek <grzesiul1995@gmail.com>
2021-09-28 19:42:31 +0100

    Add support for changing folders to index.html thingies
    I am not sure what exactly this is called, but...
    `localhost:80/folder/ --> www/folder/index.html`
    Hopefully that clears something up?

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const fs = require("fs")
 const conf = require("./config.json")
 
 http.createServer(function (req, res) {
+    if(req.url.endsWith("/")) req.url += "index.html"
+
+    console.log(`${req.socket.localAddress} GET ${req.url}`)
+
     fs.readFile(__dirname + "/www" + req.url, function (err,data) {
       if (err) {
         res.writeHead(404);
