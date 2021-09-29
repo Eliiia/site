88aca98ee31014e71eaba6f24858f4c3d0d5dade
Grzesiekek <grzesiul1995@gmail.com>
2021-09-29 22:07:42 +0100

    easy-to-change directory and IP logger

--- main.js
+++ main.js
@@ -3,19 +3,26 @@ const fs = require("fs")
 
 const conf = require("./config.json")
 
+let dir
+
+if(conf.fulldir == "") dir = __dirname
+else dir = conf.fulldir
+
 http.createServer((req, res) => {
     if(req.url.endsWith("/")) req.url += "index.html"
 
-    console.log(`${req.socket.localAddress} ${req.method} ${req.url}`)
+    if(conf.knownIPs.hasOwnProperty(req.socket.remoteAddress)) addr = conf.knownIPs[req.socket.remoteAddress]
+    else addr = req.socket.remoteAddress
+    console.log(`${addr} ${req.method} ${req.url}`)
 
     if(req.method == "GET") {
-        fs.readFile(__dirname + conf.dir + req.url, (err,data) => {
+        fs.readFile(dir + conf.dir + req.url, (err,data) => {
             res.setHeader('Content-Type', 'text/html');
 
             if (err) {
                 res.writeHead(404);
 
-                fs.readFile(__dirname + "/other/error.html", (err,data) => {
+                fs.readFile(dir + "/other/error.html", (err,data) => {
                     res.end(data)
                 })
                 //res.end(JSON.stringify(err))
@@ -30,8 +37,8 @@ http.createServer((req, res) => {
     if(req.method == "POST") {
         res.writeHead(405);
 
-        fs.readFile(__dirname + "/other/post.html", (err,data) => {
+        fs.readFile(dir + "/other/post.html", (err,data) => {
             res.end(data)
         })
     }
-}).listen(conf.port)
+}).listen(conf.port, conf.hostname)
