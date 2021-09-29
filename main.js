acc9d4b94256a586084f676ccfdddea465815c4d
Grzesiekek <grzesiul1995@gmail.com>
2021-09-29 20:20:49 +0100

    POST request stuff

--- main.js
+++ main.js
@@ -6,22 +6,32 @@ const conf = require("./config.json")
 http.createServer((req, res) => {
     if(req.url.endsWith("/")) req.url += "index.html"
 
-    console.log(`${req.socket.localAddress} GET ${req.url}`)
+    console.log(`${req.socket.localAddress} ${req.method} ${req.url}`)
 
-    fs.readFile(__dirname + conf.dir + req.url, (err,data) => {
-        res.setHeader('Content-Type', 'text/html');
+    if(req.method == "GET") {
+        fs.readFile(__dirname + conf.dir + req.url, (err,data) => {
+            res.setHeader('Content-Type', 'text/html');
 
-        if (err) {
-            res.writeHead(404);
+            if (err) {
+                res.writeHead(404);
 
-            fs.readFile(__dirname + "/other/error.html", (err,data) => {
-                res.end(data)
-            })
-            //res.end(JSON.stringify(err))
-            return
-        }
+                fs.readFile(__dirname + "/other/error.html", (err,data) => {
+                    res.end(data)
+                })
+                //res.end(JSON.stringify(err))
+                return
+            }
 
-        res.writeHead(200)
-        res.end(data)
-    });
+            res.writeHead(200)
+            res.end(data)
+        });
+    }
+
+    if(req.method == "POST") {
+        res.writeHead(405);
+
+        fs.readFile(__dirname + "/other/post.html", (err,data) => {
+            res.end(data)
+        })
+    }
 }).listen(conf.port)
