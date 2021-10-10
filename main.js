e62025b7f32a1ce5448ad3a1827cb09d66dc2242
Grzesiekek <grzesiul1995@gmail.com>
2021-10-10 23:13:23 +0100

    file uploads! (and time on logs)

--- main.js
+++ main.js
@@ -14,7 +14,12 @@ http.createServer((req, res) => {
 
     if(conf.knownIPs.hasOwnProperty(req.socket.remoteAddress)) addr = conf.knownIPs[req.socket.remoteAddress]
     else addr = req.socket.remoteAddress
-    console.log(`${addr} ${req.method} ${req.url}`)
+
+    d = new Date(); h = d.getHours(); m = d.getMinutes(); s = d.getSeconds()
+    if((h+"").length === 1) h = "0" + h
+    if((m+"").length === 1) m = "0" + m
+    if((s+"").length === 1) s = "0" + s
+    console.log(`[${h}:${m}:${s}] ${addr} ${req.method} ${req.url}`)
 
     if(req.method == "GET") {
         if(req.url.endsWith("/")) req.url += "index.html"
@@ -47,23 +52,32 @@ http.createServer((req, res) => {
         let body = "";
 
         req.on("data", (chunk) => {
-            body += `${chunk}`
-            
+            body += chunk
+
             if(body.length > 1e6) res.end() // yes, i know i need a proper way of doing this but whatever
         })
 
         req.on("end", async () => {
-            console.log("\tPOST request finished")
-
             if(req.url == "/ip") { 
                 if(conf.knownIPs[req.socket.remoteAddress]) {
                     console.log("\tlmao")
-                    return res.end()
                 }
 
-                let s = body.split("=")
+                let s = body.toString().split("=")
                 conf.knownIPs[addr] = s[1]
                 fs.writeFileSync("./config.json", JSON.stringify(conf, null, 4))
+            }
+            else if(req.url == "/upload/") {
+                if(!conf.whitelist.includes(req.socket.remoteAddress)) return res.end()
+
+                body = body.split("\n")
+                let filename = body[1].split('"')[3]
+                delete body[0]; delete body[1]; delete body[2]; delete body[body.length -2]
+                body = body.join("\n")
+                
+                console.log("\tfile uploaded")
+
+                fs.writeFileSync(`${dir}${conf.dir}/downloads/${filename}`, body, "utf-8", {recursive:true})
             }
             else {
                 res.writeHead(405);
@@ -73,7 +87,9 @@ http.createServer((req, res) => {
                 })
             }
 
-            res.end("thank u.")
+            console.log("\tPOST request finished")
+
+            res.end()
         })
     }
 }).listen(conf.port, conf.hostname)
